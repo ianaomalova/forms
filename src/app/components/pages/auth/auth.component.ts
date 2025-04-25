b1760b35411634de5c7246ff88052d4411515858
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-04-25 11:40:01 +0300

    added validators

--- src/app/components/pages/auth/auth.component.ts
+++ src/app/components/pages/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {NgSwitch, NgSwitchCase, NgTemplateOutlet} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {NgClass, NgSwitch, NgSwitchCase, NgTemplateOutlet} from '@angular/common';
 import { trigger, transition, style, animate } from '@angular/animations';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 
 @Component({
@@ -8,7 +9,10 @@ import { trigger, transition, style, animate } from '@angular/animations';
   imports: [
     NgSwitch,
     NgSwitchCase,
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    NgClass
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
@@ -28,11 +32,60 @@ import { trigger, transition, style, animate } from '@angular/animations';
   ]
 })
 
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   mode: 'login' | 'register' = 'login';
+
+  loginForm!: FormGroup;
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForms();
+  }
+
+  initForms(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator
+    })
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return (password && confirmPassword && password !== confirmPassword) ? {passwordMismatch: true} : null;
+  }
+
+  get passwordMatch() {
+    return this.registerForm.hasError('passwordMismatch');
+  }
 
   toggleMode(event: Event, mode: 'login' | 'register') {
     event.preventDefault();
     this.mode = mode;
+  }
+
+  onSubmitLogin() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm);
+  }
+
+  onSubmitRegister() {
+    console.log(this.registerForm);
   }
 }
