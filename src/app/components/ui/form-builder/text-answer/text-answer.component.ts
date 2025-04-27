968492d82855b29d7aa073c0c2bf58dccfe0826c
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-04-27 15:52:58 +0300

    feat: добавлен попап для копирования блока форма

--- src/app/components/ui/form-builder/text-answer/text-answer.component.ts
+++ src/app/components/ui/form-builder/text-answer/text-answer.component.ts
@@ -1,27 +1,48 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ToggleSwitch} from 'primeng/toggleswitch';
 import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {FormElementType} from '../../../../models/formElement.interface';
-import {NgClass, NgStyle} from '@angular/common';
+import {NgClass} from '@angular/common';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-text-answer',
   imports: [
-    ToggleSwitch,
     FormsModule,
     ReactiveFormsModule,
-    NgStyle,
     NgClass
   ],
   templateUrl: './text-answer.component.html',
-  styleUrl: './text-answer.component.scss'
+  styleUrl: './text-answer.component.scss',
+  animations: [
+    trigger('menuAnimation', [
+      state('hidden', style({
+        opacity: 0,
+        transform: 'scale(0.9)'
+      })),
+      state('visible', style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      transition('hidden => visible', [
+        animate('350ms ease-out')
+      ]),
+      transition('visible => hidden', [
+        animate('350ms ease-in')
+      ]),
+    ]),
+  ]
 })
 export class TextAnswerComponent implements OnInit {
   @Input() typeAnswer: FormElementType = FormElementType.SINGLE_TEXT;
   @Output() dragStarted = new EventEmitter<DragEvent>();
   @Output() deleteRequested = new EventEmitter<void>();
+  @Output() copyRequested = new EventEmitter<string>();
 
   form!: FormGroup;
+
+  showCopyMenu = false;
+  copyMenuPosition = { top: '0', left: '0' };
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -44,6 +65,10 @@ export class TextAnswerComponent implements OnInit {
     this.deleteRequested.emit();
   }
 
+  onCopy(position: string) {
+    this.copyRequested.emit(position);
+  }
+
   get options(): FormArray {
     return this.form.get('options') as FormArray;
   }
@@ -58,6 +83,25 @@ export class TextAnswerComponent implements OnInit {
 
   onDrugStarted(event: DragEvent) {
     this.dragStarted.emit(event);
+  }
+
+  openCopyMenu(event: MouseEvent) {
+    this.showCopyMenu = true;
+    this.copyMenuPosition = {
+      top: `${event.clientY - 10}px`,
+      left: `${event.clientX - 10}px`
+    }
+
+    event.stopPropagation();
+  }
+
+  closeCopyMenu() {
+    this.showCopyMenu = false;
+  }
+
+  copyElement(position: 'above'|'below'| 'bottom' ) {
+    this.closeCopyMenu();
+    this.copyRequested.emit(position);
   }
 
   checked: boolean = false;
